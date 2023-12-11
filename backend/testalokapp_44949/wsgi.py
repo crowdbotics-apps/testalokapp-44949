8baebfff94c8e4d3f194cc2609658fb476f26adc
Crowdbotics <team@crowdbotics.com>
2023-12-11 12:22:20 +0000

    Initial commit

---
+++ backend/testalokapp_44949/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for testalokapp_44949 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "testalokapp_44949.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testalokapp-44949",
+  url: "https://testalokapp-44949.botics.co",
+  api: "https://testalokapp-44949.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
